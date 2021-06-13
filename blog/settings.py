b8882c51e33aa50450a1a1d483360a4f4c74e950
Dregor <yusufoff.is@yandex.ru>
2021-06-13 14:34:17 +0300

    up

--- blog/settings.py
+++ blog/settings.py
@@ -123,9 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'                                                      #Урл медиа директории
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
