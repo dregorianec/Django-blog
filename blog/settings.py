7f654416c42547fade36518eaf52fd348e2c624e
dregorianec <58620817+dregorianec@users.noreply.github.com>
2021-06-13 17:48:54 +0300

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'PIL',
+    'Pillow',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
